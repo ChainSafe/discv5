0b95995e26235cae56d0b042fdb6b0e84d94b94c
Mikerah <mikerahqc@protonmail.com>
2019-07-15 10:01:49 -0400

    Updated messages with an auth packet

--- src/messages.ts
+++ src/messages.ts
@@ -13,17 +13,31 @@ export interface IPacket {
 /*
  * Packet messages
  */
-export interface IRandomPacket extends IPacket{
+export interface IRandomPacket extends IPacket {
   random_packet: Buffer;
   auth_tag: Buffer;
   random_data: Buffer;
 }
 
-export interface IWhoAreYou extends IPacket{
+export interface IWhoAreYou extends IPacket {
   whoareyou_packet: Buffer;
   magic: Buffer;
   token: Buffer;
   enr_seq: bigint;
+} 
+
+export interface IAuthPacket extends IPacket {
+  message_packet: Buffer;
+  auth_header: Buffer[];
+  auth_scheme_name: string;
+  auth_response: Buffer;
+  auth_response_pt: Buffer[];
+  zero_nonce: Buffer;
+  id_nonce_sig: Buffer;
+  static_node_key: Buffer;
+  message: Buffer;
+  message_pt: Buffer;
+  auth_tag: Buffer;
 }
 
 /**
@@ -48,10 +62,6 @@ export interface IMessagePacketHandshake extends IPacket{
   message_packet: Buffer;
   message: Buffer;
 }
-
-type packet = IPacket | IRandomPacket | IWhoAreYou | IMessagePacket | IMessagePacketHandshake;
-type request = IPing | IFindNode | IReqTicket | IRegTopic | ITopicQuery;
-type response = IPong | INodes | ITicket | IRegConfirmation;
 
 /**
  * Protocol Messages
@@ -123,4 +133,33 @@ export interface ITopicQuery {
   topic: Buffer;
 }
 
+type packet = IPacket | IRandomPacket | IWhoAreYou | IAuthPacket |IMessagePacket | IMessagePacketHandshake;
+type request = IPing | IFindNode | IReqTicket | IRegTopic | ITopicQuery;
+type response = IPong | INodes | ITicket | IRegConfirmation;
 
+// Type mappings 
+export const typeMaps = {
+  byType = {
+    0x01: 'PING',
+    0x02: 'PONG',
+    0x03: 'FINDNODE',
+    0x04: 'NODES',
+    0x05: 'REQTICKET',
+    0x06: 'TICKET',
+    0x07: 'REGTOPIC',
+    0x08: 'REGCONFIRMATION',
+    0x09: 'TOPICQUERY'
+  },
+
+  byName = {
+    'PING': 0x01,
+    'PONG': 0x02,
+    'FINDNODE': 0x03,
+    'NODES': 0x04,
+    'REQTICKET': 0x05,
+    'TICKET': 0x06,
+    'REGTOPIC': 0x07,
+    'REGCONFIRMATION': 0x08,
+    'TOPICQUERY': 0x09
+  }
+};
