2f7fecb34a4fd94758e3d9119e2722b4f2f8335c
Tuyen Nguyen <vutuyen2636@gmail.com>
2020-09-25 13:14:20 +0700

    Fix enr api

--- test/tuyen/runDiscv5.ts
+++ test/tuyen/runDiscv5.ts
@@ -2,7 +2,7 @@
 import { readEnr, readPeerId } from "./util";
 import { Discv5Discovery } from "../../src/libp2p/discv5";
 import { ENR } from "../../src/enr";
-import Multiaddr from "multiaddr";
+// import Multiaddr from "multiaddr";
 
 
 async function main(): Promise<void> {
@@ -13,7 +13,9 @@ async function main(): Promise<void> {
   const bindAddr = "/ip4/0.0.0.0/udp/9001";
   // const bootEnr = await readEnr("/Users/tuyennguyen/Projects/workshop/lodestar_fresh/lodestar/packages/lodestar-cli/.medalla/enr.json");
   const bootEnr = await ENR.decodeTxt("enr:-IO4QAYORrTjQl6z6A6XYLpgmXVmawH5LqMkobVTEkOCeNU-Oc9FS5A0GP02xsmlRS4uRZYtngNNZDiagMQnnpllbFYCgmlkgnY0iXNlY3AyNTZrMaECI_HdAJQwUq2cT2qNTjcTqmnmigKAEVUaM_4l2AXjgTiDdGNwgiMog3VkcIIjKA")
-  bootEnr.setLocationMultiaddr(Multiaddr("/ip4/192.168.100.26/udp/9000"))
+  // bootEnr.setLocationMultiaddr(Multiaddr("/ip4/192.168.100.26/udp/9000"))
+  bootEnr.ip = "192.168.100.26";
+  bootEnr.udp = 9000;
   const discv5 = new Discv5Discovery({
     bindAddr,
     bootEnrs: [bootEnr],
