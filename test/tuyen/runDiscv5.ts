0812e1aac6c3acee971efdeae6bae8248e6e9232
Tuyen Nguyen <vutuyen2636@gmail.com>
2020-09-26 05:20:14 +0700

    Try 2nd boot enr

--- test/tuyen/runDiscv5.ts
+++ test/tuyen/runDiscv5.ts
@@ -12,9 +12,9 @@ async function main(): Promise<void> {
   // 9000 for rumor
   const bindAddr = "/ip4/0.0.0.0/udp/9001";
   // const bootEnr = await readEnr("/Users/tuyennguyen/Projects/workshop/lodestar_fresh/lodestar/packages/lodestar-cli/.medalla/enr.json");
-  const bootEnr = await ENR.decodeTxt("enr:-IO4QE-_6sPGVev16Z7z3upgbpG1gOeXUAuy2N9UVr6qW2DIZtPUWd4gbvyXwOojVMtuzf1uy3E3LrpT17lV3S8wx9ICgmlkgnY0iXNlY3AyNTZrMaECrnFB2_27QnaVppdvDTENeTv_Q6ybtNnPXCqHNSMcxT-DdGNwgiMog3VkcIIjKA")
+  const bootEnr = await ENR.decodeTxt("enr:-Ku4QLglCMIYAgHd51uFUqejD9DWGovHOseHQy7Od1SeZnHnQ3fSpE4_nbfVs8lsy8uF07ae7IgrOOUFU0NFvZp5D4wBh2F0dG5ldHOIAAAAAAAAAACEZXRoMpAYrkzLAAAAAf__________gmlkgnY0gmlwhBLf22SJc2VjcDI1NmsxoQJxCnE6v_x2ekgY_uoE1rtwzvGy40mq9eD66XfHPBWgIIN1ZHCCD6A")
   // bootEnr.setLocationMultiaddr(Multiaddr("/ip4/192.168.100.26/udp/9000"))
-  bootEnr.ip = "161.35.29.249";
+  // bootEnr.ip = "161.35.29.249";
   bootEnr.udp = 9000;
   const discv5 = new Discv5Discovery({
     bindAddr,
