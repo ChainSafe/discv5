ChainSafe/discv5

commit 122adbf00734ab03bba7bc47abafafc69ca1ef23

feat: Add IP based rate limiting (#206)

src/service/service.ts
@@ -50,6 +50,7 @@ import {
   IDiscv5Metrics,
   INodesResponse,
 } from "./types.js";
+import { RateLimiter, RateLimiterOpts } from "../rateLimit/index.js";
 
 const log = debug("discv5:service");
 
@@ -75,6 +76,10 @@ export interface IDiscv5CreateOptions {
   config?: Partial<IDiscv5Config>;
   metrics?: IDiscv5Metrics;
   transport?: ITransportService;
+  /**
+   * Enable optional packet rate limiter with opts
+   */
+  rateLimiterOpts?: RateLimiterOpts;
 }
 
 /**
@@ -184,15 +189,16 @@ export class Discv5 extends (EventEmitter as { new (): Discv5EventEmitter }) {
    * @param peerId the PeerId with the keypair that identifies the enr
    * @param multiaddr The multiaddr which contains the the network interface and port to which the UDP server binds
    */
-  public static create({ enr, peerId, multiaddr, config = {}, metrics, transport }: IDiscv5CreateOptions): Discv5 {
+  public static create(opts: IDiscv5CreateOptions): Discv5 {
+    const { enr, peerId, multiaddr, config = {}, metrics, transport } = opts;
     const fullConfig = { ...defaultConfig, ...config };
     const decodedEnr = typeof enr === "string" ? ENR.decodeTxt(enr) : enr;
-    const sessionService = new SessionService(
-      fullConfig,
-      decodedEnr,
-      createKeypairFromPeerId(peerId),
-      transport ?? new UDPTransportService(multiaddr, decodedEnr.nodeId)
-    );
+    const rateLimiter = opts.rateLimiterOpts && new RateLimiter(opts.rateLimiterOpts, metrics ?? null);
+    const sessionService = new SessionService(fullConfig, {
+      enr: decodedEnr,
+      keypair: createKeypairFromPeerId(peerId),
+      transport: transport ?? new UDPTransportService(multiaddr, decodedEnr.nodeId, rateLimiter),
+    });
     return new Discv5(fullConfig, sessionService, metrics);
   }
 

src/session/service.ts
@@ -41,8 +41,16 @@ import {
 import { getNodeAddress, INodeAddress, INodeContactType, nodeAddressToString, NodeContact } from "./nodeInfo.js";
 import LRUCache from "lru-cache";
 import { TimeoutMap } from "../util/index.js";
+import { IDiscv5Metrics } from "../service/types.js";
 
 const log = debug("discv5:sessionService");
+
+export interface SessionServiceOpts {
+  enr: ENR;
+  keypair: IKeypair;
+  transport: ITransportService;
+  metrics?: IDiscv5Metrics;
+}
 
 /**
  * Session management for the Discv5 Discovery service.
@@ -126,8 +134,11 @@ export class SessionService extends (EventEmitter as { new (): StrictEventEmitte
    */
   private sessions: LRUCache<NodeAddressString, Session>;
 
-  constructor(config: ISessionConfig, enr: ENR, keypair: IKeypair, transport: ITransportService) {
+  constructor(config: ISessionConfig, opts: SessionServiceOpts) {
     super();
+
+    const { enr, keypair, transport } = opts;
+
     // ensure the keypair matches the one that signed the ENR
     if (!keypair.publicKey.equals(enr.publicKey)) {
       throw new Error("Provided keypair does not match the provided ENR keypair");
@@ -136,6 +147,7 @@ export class SessionService extends (EventEmitter as { new (): StrictEventEmitte
     this.enr = enr;
     this.keypair = keypair;
     this.transport = transport;
+
     this.activeRequests = new TimeoutMap(config.requestTimeout, (k, v) =>
       this.handleRequestTimeout(getNodeAddress(v.contact), v)
     );
@@ -282,6 +294,7 @@ export class SessionService extends (EventEmitter as { new (): StrictEventEmitte
     const challengeData = encodeChallengeData(packet.maskingIv, packet.header);
 
     log("Sending WHOAREYOU to %o", nodeAddr);
+    this.addExpectedResponse(nodeAddr.socketAddr);
     this.send(nodeAddr, packet);
 
     this.activeChallenges.set(nodeAddrStr, { data: challengeData, remoteEnr: remoteEnr ?? undefined });
@@ -744,14 +757,12 @@ export class SessionService extends (EventEmitter as { new (): StrictEventEmitte
     }
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   private removeExpectedResponse(socketAddr: Multiaddr): void {
-    //
+    this.transport.addExpectedResponse(socketAddr.toOptions().host);
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   private addExpectedResponse(socketAddr: Multiaddr): void {
-    //
+    this.transport.removeExpectedResponse(socketAddr.toOptions().host);
   }
 
   private handleRequestTimeout(nodeAddr: INodeAddress, requestCall: IRequestCall): void {

src/transport/types.ts
@@ -29,4 +29,9 @@ export interface ITransportService extends TransportEventEmitter {
   start(): Promise<void>;
   stop(): Promise<void>;
   send(to: Multiaddr, toId: string, packet: IPacket): Promise<void>;
+
+  /** Add 1 expected response of unknown lenght from IP for rate limiter */
+  addExpectedResponse(ipAddress: string): void;
+  /** Remove 1 expected response from IP added with addExpectedResponse */
+  removeExpectedResponse(ipAddress: string): void;
 }

test/unit/session/service.test.ts
@@ -42,8 +42,8 @@ describe("session service", () => {
     transport0 = new UDPTransportService(addr0, enr0.nodeId);
     transport1 = new UDPTransportService(addr1, enr1.nodeId);
 
-    service0 = new SessionService(defaultConfig, enr0, kp0, transport0);
-    service1 = new SessionService(defaultConfig, enr1, kp1, transport1);
+    service0 = new SessionService(defaultConfig, { enr: enr0, keypair: kp0, transport: transport0 });
+    service1 = new SessionService(defaultConfig, { enr: enr1, keypair: kp1, transport: transport1 });
 
     await service0.start();
     await service1.start();

src/service/types.ts
@@ -128,4 +128,9 @@ export interface IDiscv5Metrics {
   sentMessageCount: IGauge<"type">;
   /** Total number messages received by message type */
   rcvdMessageCount: IGauge<"type">;
+
+  /** Total count of rate limit hits by IP */
+  rateLimitHitIP: IGauge;
+  /** Total count of rate limit hits by total requests */
+  rateLimitHitTotal: IGauge;
 }

src/rateLimit/index.ts
@@ -1,0 +1,80 @@
+import { IDiscv5Metrics } from "../service/index.js";
+import { RateLimiterGRCA, RateLimiterQuota } from "./rateLimiterGRCA.js";
+
+type IPAddress = string;
+
+export interface PacketSrc {
+  address: IPAddress;
+  family: "IPv4" | "IPv6";
+  port: number;
+  size: number;
+}
+
+export interface RateLimiterOpts {
+  globalQuota: RateLimiterQuota;
+  byIPQuota: RateLimiterQuota;
+}
+
+export interface IRateLimiter {
+  allowEncodedPacket(ip: IPAddress): boolean;
+  addExpectedResponse(ip: IPAddress): void;
+  removeExpectedResponse(ip: IPAddress): void;
+}
+
+export class RateLimiter implements IRateLimiter {
+  private readonly rateLimiterGlobal: RateLimiterGRCA<null>;
+  private readonly rateLimiterIP: RateLimiterGRCA<IPAddress>;
+
+  private readonly bannedIPs = new Map<IPAddress, number>();
+  private readonly expectedResponsesByIP = new Map<IPAddress, number>();
+
+  constructor(opts: RateLimiterOpts, private readonly metrics: IDiscv5Metrics | null) {
+    this.rateLimiterGlobal = RateLimiterGRCA.fromQuota(opts.globalQuota);
+    this.rateLimiterIP = RateLimiterGRCA.fromQuota(opts.byIPQuota);
+  }
+
+  allowEncodedPacket(ip: IPAddress): boolean {
+    if (this.bannedIPs.has(ip)) {
+      return false;
+    }
+
+    if (this.expectsResponseFromIP(ip)) {
+      return true;
+    }
+
+    if (!this.rateLimiterIP.allows(ip, 1)) {
+      this.metrics?.rateLimitHitIP.inc();
+
+      this.bannedIPs.set(ip, Date.now());
+
+      return false;
+    }
+
+    if (!this.rateLimiterGlobal.allows(null, 1)) {
+      this.metrics?.rateLimitHitTotal.inc();
+      return false;
+    }
+
+    return true;
+  }
+
+  addExpectedResponse(ip: IPAddress): void {
+    this.expectedResponsesByIP.set(ip, (this.expectedResponsesByIP.get(ip) ?? 0) + 1);
+  }
+
+  removeExpectedResponse(ip: IPAddress): void {
+    const expectedResponses = this.expectedResponsesByIP.get(ip);
+    if (expectedResponses !== undefined) {
+      if (expectedResponses > 1) {
+        this.expectedResponsesByIP.set(ip, expectedResponses - 1);
+      } else {
+        this.expectedResponsesByIP.delete(ip);
+      }
+    }
+  }
+
+  /** After a request initiated by us we expected a response from this IP */
+  private expectsResponseFromIP(ip: IPAddress): boolean {
+    return this.expectedResponsesByIP.has(ip);
+  }
+}

src/rateLimit/rateLimiterGRCA.ts
@@ -1,0 +1,89 @@
+type MiliSeconds = number;
+
+export interface RateLimiterQuota {
+  /** How often are `max_tokens` fully replenished. */
+  replenishAllEvery: MiliSeconds;
+  /** Token limit. This translates on how large can an instantaneous batch of tokens be. */
+  maxTokens: number;
+}
+
+/**
+ * Generic Cell Rate Algorithm is a leaky bucket-type scheduling algorithm.
+ *
+ * Most rate-limit implementations are either time-bucket or leaky-bucket based. The time-bucket requires the storage
+ * of two values and does not enforce a rate, while the leaky-bucket approach requires a separate process to
+ * continually refill the bucket. GCRA only storing a value (the TAT) while still being simple. GCRA may be rarely
+ * used because of its perceived complexity.
+ *
+ * GCRA aims to limit requests to `R = L/P`, where this implementation sets `L = 1` for simplicity. The target rate
+ * then is `R = 1/P` so request separated by at least `P` are not limited. Define the Theoretical Arrival Time (TAT)
+ * of the next request to be equal
+ */
+export class RateLimiterGRCA<Key> {
+  /** Time when the bucket will be full for each peer. TAT (theoretical arrival time) from GCRA */
+  private readonly tatPerKey = new Map<Key, MiliSeconds>();
+  private readonly startTimeMs = Date.now();
+
+  constructor(
+    /** After how long is the bucket considered full via replenishing 1T every `t`. */
+    private readonly tau: MiliSeconds,
+    /** How often is 1 token replenished */
+    private readonly t: MiliSeconds
+  ) {
+    this.tau = tau;
+    this.t = t;
+  }
+
+  static fromQuota<Key>(quota: RateLimiterQuota): RateLimiterGRCA<Key> {
+    if (quota.maxTokens === 0) {
+      throw Error("Max number of tokens should be positive");
+    }
+    const tau = quota.replenishAllEvery;
+    if (tau === 0) {
+      throw Error("Replenish time must be positive");
+    }
+    const t = tau / quota.maxTokens;
+    return new RateLimiterGRCA(tau, t);
+  }
+
+  allows(key: Key, tokens: number, msSinceStart?: number): boolean {
+    if (msSinceStart === undefined) {
+      msSinceStart = Date.now() - this.startTimeMs;
+    }
+
+    /** how long does it take to replenish these tokens */
+    const additionalTime = this.t * tokens;
+
+    if (additionalTime > this.tau) {
+      // the time required to process this amount of tokens is longer than the time that makes the bucket full.
+      return false;
+    }
+
+    // If the key is new, we consider their bucket full (which means, their request will be allowed)
+    let tat = this.tatPerKey.get(key);
+    if (tat === undefined) {
+      tat = msSinceStart;
+      this.tatPerKey.set(key, tat);
+    }
+
+    // check how soon could the request be made
+    const earliestTime = tat + additionalTime - this.tau;
+    if (msSinceStart < earliestTime) {
+      return false;
+    }
+
+    // calculate the new TAT
+    this.tatPerKey.set(key, Math.max(msSinceStart, tat) + additionalTime);
+    return true;
+  }
+
+  /** Removes keys for which their bucket is full by `time_limit` */
+  prune(timeLimit: MiliSeconds): void {
+    for (const entry of this.tatPerKey.entries()) {
+      // remove those for which tat < lim
+      if (entry[1] < timeLimit) {
+        this.tatPerKey.delete(entry[0]);
+      }
+    }
+  }
+}

src/transport/udp.ts
@@ -4,6 +4,7 @@ import { Multiaddr, multiaddr } from "@multiformats/multiaddr";
 
 import { decodePacket, encodePacket, IPacket, MAX_PACKET_SIZE } from "../packet/index.js";
 import { IRemoteInfo, ITransportService, TransportEventEmitter } from "./types.js";
+import { IRateLimiter } from "../rateLimit/index.js";
 
 /**
  * This class is responsible for encoding outgoing Packets and decoding incoming Packets over UDP
@@ -12,18 +13,18 @@ export class UDPTransportService
   extends (EventEmitter as { new (): TransportEventEmitter })
   implements ITransportService
 {
-  public multiaddr: Multiaddr;
   private socket!: dgram.Socket;
-  private srcId: string;
 
-  public constructor(multiaddr: Multiaddr, srcId: string) {
+  public constructor(
+    readonly multiaddr: Multiaddr,
+    private readonly srcId: string,
+    private readonly rateLimiter?: IRateLimiter
+  ) {
     super();
     const opts = multiaddr.toOptions();
     if (opts.transport !== "udp") {
       throw new Error("Local multiaddr must use UDP");
     }
-    this.multiaddr = multiaddr;
-    this.srcId = srcId;
   }
 
   public async start(): Promise<void> {
@@ -49,13 +50,29 @@ export class UDPTransportService
     );
   }
 
-  public handleIncoming = (data: Buffer, rinfo: IRemoteInfo): void => {
+  addExpectedResponse(ipAddress: string): void {
+    this.rateLimiter?.addExpectedResponse(ipAddress);
+  }
+
+  removeExpectedResponse(ipAddress: string): void {
+    this.rateLimiter?.removeExpectedResponse(ipAddress);
+  }
+
+  private handleIncoming = (data: Buffer, rinfo: IRemoteInfo): void => {
+    if (this.rateLimiter && !this.rateLimiter.allowEncodedPacket(rinfo.address)) {
+      return;
+    }
+
     const mu = multiaddr(`/${String(rinfo.family).endsWith("4") ? "ip4" : "ip6"}/${rinfo.address}/udp/${rinfo.port}`);
+
+    let packet: IPacket;
     try {
-      const packet = decodePacket(this.srcId, data);
-      this.emit("packet", mu, packet);
+      packet = decodePacket(this.srcId, data);
     } catch (e: unknown) {
       this.emit("decodeError", e as Error, mu);
+      return;
     }
+
+    this.emit("packet", mu, packet);
   };
 }

commit 937262bfe2d7120e6d4c943e4d6e06ef64a70da5

chore: remove breaking change introduced in #206

src/service/service.ts
@@ -194,11 +194,12 @@ export class Discv5 extends (EventEmitter as { new (): Discv5EventEmitter }) {
     const fullConfig = { ...defaultConfig, ...config };
     const decodedEnr = typeof enr === "string" ? ENR.decodeTxt(enr) : enr;
     const rateLimiter = opts.rateLimiterOpts && new RateLimiter(opts.rateLimiterOpts, metrics ?? null);
-    const sessionService = new SessionService(fullConfig, {
-      enr: decodedEnr,
-      keypair: createKeypairFromPeerId(peerId),
-      transport: transport ?? new UDPTransportService(multiaddr, decodedEnr.nodeId, rateLimiter),
-    });
+    const sessionService = new SessionService(
+      fullConfig,
+      decodedEnr,
+      createKeypairFromPeerId(peerId),
+      transport ?? new UDPTransportService(multiaddr, decodedEnr.nodeId, rateLimiter)
+    );
     return new Discv5(fullConfig, sessionService, metrics);
   }
 

src/session/service.ts
@@ -134,10 +134,8 @@ export class SessionService extends (EventEmitter as { new (): StrictEventEmitte
    */
   private sessions: LRUCache<NodeAddressString, Session>;
 
-  constructor(config: ISessionConfig, opts: SessionServiceOpts) {
+  constructor(config: ISessionConfig, enr: ENR, keypair: IKeypair, transport: ITransportService) {
     super();
-
-    const { enr, keypair, transport } = opts;
 
     // ensure the keypair matches the one that signed the ENR
     if (!keypair.publicKey.equals(enr.publicKey)) {
@@ -758,11 +756,11 @@ export class SessionService extends (EventEmitter as { new (): StrictEventEmitte
   }
 
   private removeExpectedResponse(socketAddr: Multiaddr): void {
-    this.transport.addExpectedResponse(socketAddr.toOptions().host);
+    this.transport.addExpectedResponse?.(socketAddr.toOptions().host);
   }
 
   private addExpectedResponse(socketAddr: Multiaddr): void {
-    this.transport.removeExpectedResponse(socketAddr.toOptions().host);
+    this.transport.removeExpectedResponse?.(socketAddr.toOptions().host);
   }
 
   private handleRequestTimeout(nodeAddr: INodeAddress, requestCall: IRequestCall): void {

src/transport/types.ts
@@ -31,7 +31,7 @@ export interface ITransportService extends TransportEventEmitter {
   send(to: Multiaddr, toId: string, packet: IPacket): Promise<void>;
 
   /** Add 1 expected response of unknown lenght from IP for rate limiter */
-  addExpectedResponse(ipAddress: string): void;
+  addExpectedResponse?(ipAddress: string): void;
   /** Remove 1 expected response from IP added with addExpectedResponse */
-  removeExpectedResponse(ipAddress: string): void;
+  removeExpectedResponse?(ipAddress: string): void;
 }

test/unit/session/service.test.ts
@@ -42,8 +42,8 @@ describe("session service", () => {
     transport0 = new UDPTransportService(addr0, enr0.nodeId);
     transport1 = new UDPTransportService(addr1, enr1.nodeId);
 
-    service0 = new SessionService(defaultConfig, { enr: enr0, keypair: kp0, transport: transport0 });
-    service1 = new SessionService(defaultConfig, { enr: enr1, keypair: kp1, transport: transport1 });
+    service0 = new SessionService(defaultConfig, enr0, kp0, transport0);
+    service1 = new SessionService(defaultConfig, enr1, kp1, transport1);
 
     await service0.start();
     await service1.start();