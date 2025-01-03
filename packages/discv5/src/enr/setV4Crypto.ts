b6b9bf2701f3345f98a8030a4b0fb6ebc7416284
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2025-01-03 14:41:57 -0500

    Update bcrypto references

--- packages/discv5/src/enr/setV4Crypto.ts
+++ packages/discv5/src/enr/setV4Crypto.ts
@@ -1,4 +1,6 @@
-// This module has the side effect of setting the ENR crypto implementations to use bcrypto
+// This module has the side effect of setting the ENR crypto implementations to use the defaultCrypto
+// implementation at present.
+// TODO: Adjust this if we use a different implementation
 
 import { setV4Crypto, defaultCrypto } from "@chainsafe/enr";
 
