548860ccae0ac97d1be62e23f7b9b8a8c04560d0
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2025-01-02 20:49:03 -0500

    remove bcryptoV4 interface

--- packages/discv5/src/enr/setV4Crypto.ts
+++ packages/discv5/src/enr/setV4Crypto.ts
@@ -1,6 +1,5 @@
 // This module has the side effect of setting the ENR crypto implementations to use bcrypto
 
-import { setV4Crypto } from "@chainsafe/enr";
-import * as bcryptoV4Crypto from "./bcryptoV4Crypto.js";
+import { setV4Crypto, defaultCrypto } from "@chainsafe/enr";
 
-setV4Crypto(bcryptoV4Crypto);
+setV4Crypto(defaultCrypto);
