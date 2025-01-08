d3fd3bf327135116d4c4a850e6e3f4c31b192c31
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2025-01-08 15:13:39 -0500

    remove crypto setup

--- packages/discv5/src/enr/setV4Crypto.ts
+++
@@ -1,7 +0,0 @@
-// This module has the side effect of setting the ENR crypto implementations to use the defaultCrypto
-// implementation at present.
-// TODO: Adjust this if we use a different implementation
-
-import { setV4Crypto, defaultCrypto } from "@chainsafe/enr";
-
-setV4Crypto(defaultCrypto);
