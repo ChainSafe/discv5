63cc07691a06451dde33caa0f23f957a95245fc6
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2025-01-08 15:17:08 -0500

    clean up side effect

--- packages/discv5/src/index.ts
+++ packages/discv5/src/index.ts
@@ -5,6 +5,3 @@ export * from "./service/index.js";
 export * from "./session/index.js";
 export * from "./transport/index.js";
 export * from "./util/index.js";
-
-// side effect: set the enr crypto implementation
-import "./enr/setV4Crypto.js";
